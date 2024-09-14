4e749866fa0f4f26b0332eeea0152538a25ccc1d
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-09-14 22:09:50 +0200

    Custom Bootstrap Theme from the mentor

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,24 +1,26 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
+import logo from "../assets/logo.png";
 
 const Header = () => {
   return (
     <div>
       <header>
         <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
-          {/* Container component to center the content */}
           <Container>
-            {/* Navbar.Brand component for the website logo */}
-            <Navbar.Brand href="/">ProShop</Navbar.Brand>
-            {/* Navbar.Toggle component for toggling the navbar on small screens */}
+            <Navbar.Brand href="/">
+              <img src={logo} alt="ProShop Logo" />
+              ProShop
+            </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            {/* Navbar.Collapse component for collapsing the navbar on small screens */}
             <Navbar.Collapse id="basic-navbar-nav">
-              {/* Nav component for navigation links */}
               <Nav className="ms-auto">
-                {/* Nav.Link component for the cart link */}
-                <Nav.Link href="/cart"><FaShoppingCart /> Cart</Nav.Link>
-                <Nav.Link href="/login"><FaUser /> Sign In</Nav.Link>
+                <Nav.Link href="/cart">
+                  <FaShoppingCart /> Cart
+                </Nav.Link>
+                <Nav.Link href="/login">
+                  <FaUser /> Sign In
+                </Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Container>
