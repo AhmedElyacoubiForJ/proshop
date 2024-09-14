51b485c81ae3d871e3be9c3c873bd0cce8c0f760
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-09-14 23:21:08 +0200

    products listing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,7 @@ const App = () => {
         </Container>
       </main>
       <Footer />
-      </>
+    </>
   );
 };
 
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,19 +1,20 @@
 import { Row, Col } from "react-bootstrap";
-import products from '../products'
+import products from "../products";
+import Product from "../components/Product";
 
 const HomeScreen = () => {
   return (
-  <>
-    <h1>Latest Products</h1>
-    <Row>
-        {products.map(product => (
-            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                <h3>{product.name}</h3>
-            </Col>
+    <>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
         ))}
-    </Row>
-  </>
-)
+      </Row>
+    </>
+  );
 };
 
 export default HomeScreen;
