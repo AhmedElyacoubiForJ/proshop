c96dd2661520e95722b05290cf7f62fd7657fb55
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-09-24 12:35:36 +0200

    added loadinf message to homeScreen & cors problem w. using cors dependency in server

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,37 +1,28 @@
-//import { useEffect, useState } from "react";
-//import axios from "axios";
-import { useGetProductsQuery } from '../slices/productsApiSlice';
+import { useGetProductsQuery } from "../slices/productsApiSlice";
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
 
 const HomeScreen = () => {
-  //const [products, setProducts] = useState([]);
   const { data: products, isLoading, error } = useGetProductsQuery();
-
-  // useEffect(() => {
-  //   const fetchProducts = async () => {
-  //     try {
-  //       const { data } = await axios.get("/api/products");
-  //       setProducts(data);
-  //     } catch (error) {
-  //       console.error(error);
-  //     }
-  //   };
-  //   fetchProducts();
-  // }, []);
-
-  if (!products) return <h2>Loading...</h2>;
 
   return (
     <>
-      <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : error ? (
+        <div>{error?.data?.message || error.error}</div>
+      ) : (
+        <>
+          <h1>Latest Products</h1>
+          <Row>
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+        </>
+      )}
     </>
   );
 };
