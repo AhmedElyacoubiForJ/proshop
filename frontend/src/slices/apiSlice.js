4b7209c076ded353ad5786c2b4a0e45c9d61c13a
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-09-25 15:03:10 +0200

    added some comments

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -3,8 +3,10 @@ import { BASE_URL } from "../constants";
 
 const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
 
+// is a parent slice, API slice, to deal with our backend API requests
+// in an asyncchronous manner
 export const apiSlice = createApi({
   baseQuery,
-  tagTypes: ["Product", "Order", "User"],
-  endpoints: (builder) => ({}),
+  tagTypes: ["Product", "Order", "User"], // types of data that will be fetched from the our backend API
+  endpoints: (builder) => ({}), // requests building comming in separate files
 });
--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -1,12 +1,14 @@
 import { PRODUCTS_URL } from "../constants";
 import { apiSlice } from "./apiSlice";
 
+// added endpoints to the apiSlice, getProducts and getProductDetails
 export const productsApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getProducts: builder.query({
       query: () => ({
         url: PRODUCTS_URL,
       }),
+      // keep the data in the cache for 5 seconds
       keepUnusedDataFor: 5,
     }),
     getProductDetails: builder.query({
@@ -17,6 +19,6 @@ export const productsApiSlice = apiSlice.injectEndpoints({
     }),
   }),
 });
-
+// we use the convention that is used in react-query library "use" + endpoint name + "Query"
 export const { useGetProductsQuery, useGetProductDetailsQuery } =
   productsApiSlice;
